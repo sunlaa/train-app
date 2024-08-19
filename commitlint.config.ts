4fc82f47e866f878f520c37737bca0caa75a4e14
HNY_Badger <markelov-02@list.ru>
2024-08-20 02:16:55 +0400

    TA-4 fix: enable commitlint checks

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,18 +2,13 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-
-  rules: {
-    'subject-empty': [0, 'always'],
-    'type-empty': [0, 'always'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^TA-[0-9]{1,4}\s(feat|fix|init|refactor):\s(.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+      issuePrefixes: ['^TA-[0-9]{1,4}'],
+    },
   },
-  //   parserPreset: {
-  //     parserOpts: {
-  //       headerPattern: /^[TA]-[0-9]-(feat|fix|init|refactor):-.+$/,
-  //       headerCorrespondence: ['type', 'subject'],
-  //       issuePrefixes: ['^[TA]-[0-9]'],
-  //     },
-  //   },
 };
 
 export default Configuration;
