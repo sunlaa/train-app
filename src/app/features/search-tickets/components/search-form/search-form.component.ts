b149977a0cf2b66c5f361e13649fdd64110b2bdd
Uladzislava <santalova.lada25@gmail.com>
2024-08-20 19:54:21 +0200

    TA-5 feat: create validator for date input, add initial values

--- src/app/features/search-tickets/components/search-form/search-form.component.ts
+++ src/app/features/search-tickets/components/search-form/search-form.component.ts
@@ -1,14 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Subject, takeUntil } from 'rxjs';
+import { correctDate } from '../../form-validators/correct-date';
 
 @Component({
   selector: 'app-search-form',
@@ -19,28 +18,65 @@ import { provideNativeDateAdapter } from '@angular/material/core';
     MatAutocompleteModule,
     MatInputModule,
     MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.scss',
 })
-export class SearchFormComponent implements OnInit {
-  searchForm!: FormGroup;
+export class SearchFormComponent implements OnInit, OnDestroy {
+  destroy$: Subject<void> = new Subject();
 
   fb: FormBuilder = inject(FormBuilder);
 
   options = ['Warsaw', 'Minsk', 'Madrid', 'Paris'];
 
+  searchForm = this.fb.group({
+    from: ['', Validators.required],
+    to: ['', Validators.required],
+    date: [this.initDateValue, [Validators.required, correctDate]],
+    time: [{ value: '00:00', disabled: true }],
+  });
+
+  get date() {
+    return this.searchForm.controls.date;
+  }
+
+  get time() {
+    return this.searchForm.controls.time;
+  }
+
+  get initDateValue() {
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    return date;
+  }
+
+  ngOnInit() {
+    this.date.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      if (
+        !this.date.hasError('correctDate') &&
+        !this.date.hasError('required')
+      ) {
+        this.time.enable();
+      } else {
+        this.time.disable();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   dateFilter = (date: Date | null) => {
     const now = new Date();
 
-    return (date || new Date()) >= now;
+    return (date || new Date()) > now;
   };
 
-  ngOnInit() {
-    this.searchForm = this.fb.group({
-      from: ['', Validators.required],
-      to: ['', Validators.required],
-      date: '',
-    });
+  search() {
+    // console.log(this.searchForm.value);
   }
 }
