4a917bc2786b7e406d1f708d5b9114ea08125bab
HNY_Badger <markelov-02@list.ru>
2024-08-30 23:49:19 +0400

    TA-20 refactor: remove query pagination

--- src/app/features/carriages-management/components/carriages-page/carriages-page.component.ts
+++ src/app/features/carriages-management/components/carriages-page/carriages-page.component.ts
@@ -6,14 +6,13 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ConfirmationService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { PaginatorModule } from 'primeng/paginator';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { ToastModule } from 'primeng/toast';
-import { takeUntil, take } from 'rxjs';
+import { takeUntil } from 'rxjs';
 import { PageEvent } from '@/core/models/shared.model';
 import { TCarriage } from '@/core/models/carriages.model';
 import { CarriagesFacadeService } from '../../services/carriages-facade.service';
@@ -44,10 +43,6 @@ export class CarriagesPageComponent implements OnInit {
 
   private carriagesFacade = inject(CarriagesFacadeService);
 
-  private router = inject(Router);
-
-  private activatedRoute = inject(ActivatedRoute);
-
   public formToggle = false;
 
   public formCarriage: TCarriage | undefined;
@@ -65,7 +60,6 @@ export class CarriagesPageComponent implements OnInit {
   public pageCarriages: TCarriage[] = [];
 
   ngOnInit(): void {
-    this.getQueryParams();
     this.listenCarriagesState();
   }
 
@@ -82,27 +76,6 @@ export class CarriagesPageComponent implements OnInit {
       });
   }
 
-  private getQueryParams() {
-    this.activatedRoute.queryParamMap.pipe(take(1)).subscribe((params) => {
-      const pageStr = params.get('page');
-      let page = pageStr ? parseInt(pageStr, 10) : 1;
-      if (Number.isNaN(page) || page < 1) {
-        page = 1;
-      }
-      const carriagesPerPageStr = params.get('carriagesPerPage');
-      let carriagesPerPage = carriagesPerPageStr
-        ? parseInt(carriagesPerPageStr, 10)
-        : CARRIAGES_PER_PAGE_OPTIONS[0];
-      if (!CARRIAGES_PER_PAGE_OPTIONS.includes(carriagesPerPage)) {
-        [carriagesPerPage] = CARRIAGES_PER_PAGE_OPTIONS;
-      }
-      this.onPageChange({
-        first: (page - 1) * carriagesPerPage,
-        rows: carriagesPerPage,
-      });
-    });
-  }
-
   private updatePageCarriages() {
     this.pageCarriages = this.allCarriages.slice(
       this.offset,
@@ -113,8 +86,6 @@ export class CarriagesPageComponent implements OnInit {
   public onPageChange(event: PageEvent) {
     this.offset = event.first ?? this.offset;
     this.carriagesPerPage = event.rows ?? this.carriagesPerPage;
-    const page = Math.floor(this.offset / this.carriagesPerPage) + 1;
-    this.setQueryParams({ page, carriagesPerPage: this.carriagesPerPage });
     this.updatePageCarriages();
   }
 
@@ -145,13 +116,5 @@ export class CarriagesPageComponent implements OnInit {
     if (this.pageTop) {
       this.pageTop.nativeElement.scrollIntoView({ behavior: 'smooth' });
     }
-  }
-
-  private setQueryParams(params: Params) {
-    this.router.navigate([], {
-      relativeTo: this.activatedRoute,
-      queryParams: params,
-      queryParamsHandling: 'merge',
-    });
   }
 }
