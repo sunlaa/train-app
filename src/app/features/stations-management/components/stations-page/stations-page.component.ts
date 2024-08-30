faf713d563893c677d700d09c49e6b2501b30ecb
HNY_Badger <markelov-02@list.ru>
2024-08-31 00:13:27 +0400

    TA-22 refactor: remove query pagination

--- src/app/features/stations-management/components/stations-page/stations-page.component.ts
+++ src/app/features/stations-management/components/stations-page/stations-page.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { DestroyService } from '@/core/services/destroy/destroy.service';
-import { filter, take, takeUntil } from 'rxjs';
+import { filter, takeUntil } from 'rxjs';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmationService } from 'primeng/api';
 import { TStationListed } from '@/core/models/stations.model';
 import { PaginatorModule } from 'primeng/paginator';
-import { ActivatedRoute, Params, Router } from '@angular/router';
 import { PageEvent } from '@/core/models/shared.model';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { SkeletonModule } from 'primeng/skeleton';
@@ -34,10 +33,6 @@ export class StationsPageComponent implements OnInit {
 
   private stationsFacade = inject(StationsFacadeService);
 
-  private router = inject(Router);
-
-  private activatedRoute = inject(ActivatedRoute);
-
   public stationsPerPageOptions = STATIONS_PER_PAGE_OPTIONS;
 
   public offset: number = 0;
@@ -51,7 +46,6 @@ export class StationsPageComponent implements OnInit {
   public pageStations: TStationListed[] = [];
 
   ngOnInit(): void {
-    this.getQueryParams();
     this.listenStationsState();
   }
 
@@ -75,8 +69,6 @@ export class StationsPageComponent implements OnInit {
   public onPageChange(event: PageEvent) {
     this.offset = event.first ?? this.offset;
     this.stationsPerPage = event.rows ?? this.stationsPerPage;
-    const page = Math.floor(this.offset / this.stationsPerPage) + 1;
-    this.setQueryParams({ page, stationsPerPage: this.stationsPerPage });
     this.updatePageRoutes();
   }
 
@@ -85,34 +77,5 @@ export class StationsPageComponent implements OnInit {
       this.offset,
       this.stationsPerPage + this.offset,
     );
-  }
-
-  private getQueryParams() {
-    this.activatedRoute.queryParamMap.pipe(take(1)).subscribe((params) => {
-      const pageStr = params.get('page');
-      let page = pageStr ? parseInt(pageStr, 10) : 1;
-      if (Number.isNaN(page) || page < 1) {
-        page = 1;
-      }
-      const stationsPerPageStr = params.get('stationsPerPage');
-      let stationsPerPage = stationsPerPageStr
-        ? parseInt(stationsPerPageStr, 10)
-        : STATIONS_PER_PAGE_OPTIONS[0];
-      if (!STATIONS_PER_PAGE_OPTIONS.includes(stationsPerPage)) {
-        [stationsPerPage] = STATIONS_PER_PAGE_OPTIONS;
-      }
-      this.onPageChange({
-        first: (page - 1) * stationsPerPage,
-        rows: stationsPerPage,
-      });
-    });
-  }
-
-  private setQueryParams(params: Params) {
-    this.router.navigate([], {
-      relativeTo: this.activatedRoute,
-      queryParams: params,
-      queryParamsHandling: 'merge',
-    });
   }
 }
