866e56d4b6504530d47ffc2a578420d9a6a7748e
HNY_Badger <markelov-02@list.ru>
2024-08-31 19:23:23 +0400

    TA-23 feat: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ResultsComponent } from './features/search-tickets/components/results/results.component';
 import { NotificationService } from './shared/services/notification.service';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ResultsComponent],
+  imports: [RouterOutlet, ResultsComponent, HeaderComponent],
   providers: [MessageService, NotificationService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,81 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenubarModule, CommonModule, ButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent implements OnInit {
+  public items: MenuItem[] | undefined;
+
+  // TODO: Add actual data from the store
+  public email = 'email@gmail.com';
+
+  public role: 'guest' | 'user' | 'admin' = 'admin';
+
+  ngOnInit() {
+    this.initItems();
+  }
+
+  public signOut() {
+    console.log('logout');
+  }
+
+  private initItems() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: '/',
+      },
+    ];
+    if (this.role === 'guest') {
+      return;
+    }
+    this.items = [
+      ...this.items,
+      {
+        label: 'Profile',
+        icon: 'pi pi-user',
+        route: '/profile',
+      },
+      {
+        label: 'My Orders',
+        icon: 'pi pi-ticket',
+        route: '/orders',
+      },
+    ];
+    if (this.role === 'admin') {
+      this.items = [
+        ...this.items,
+        {
+          label: 'Admin',
+          icon: 'pi pi-shield',
+          items: [
+            {
+              label: 'Stations',
+              icon: 'pi pi-shop',
+              route: '/admin/stations',
+            },
+            {
+              label: 'Carriages',
+              icon: 'pi pi-warehouse',
+              route: '/admin/carriages',
+            },
+            {
+              label: 'Routes',
+              icon: 'pi pi-directions',
+              route: '/admin/routes',
+            },
+          ],
+        },
+      ];
+    }
+  }
+}
